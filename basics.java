7a5501d8bc59b399324b14b813b320e7ee4b96a0
Sonuu20 <sonugupta0208b@gmail.com>
2024-08-05 10:39:13 +0530

    basics program for testng

---
+++ basics.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class basics{
+    public static void main(String args[]){
+        // int a = 20;
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the number:");
+         int b = sc.nextInt();
+         System.out.println(b);
+        
+
+    }
+}
